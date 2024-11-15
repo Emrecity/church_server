a1f980c969d766fcf6222d3693da156f3f6df3e7
emrecity <owusu2020emmanuel@gmail.com>
2024-11-15 09:38:43 +0000

    localdb

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const port = 3000
 
 const Swagger = SwaggerDocs(app)
 
-mongoose.connect("mongodb+srv://owusu2020emmanuel:Emre@19.@cluster0.1nfp2.mongodb.net/PresbyDB?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+mongoose.connect('mongodb://localhost:27017/PresbyDb').then(()=>{
     console.log('Db connected')
     app.listen( port,()=>{
     console.log('Server is up and running')  
