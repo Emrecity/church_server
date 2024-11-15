300c71a05ca41d3060d9a3fee7832fb72b113794
emrecity <owusu2020emmanuel@gmail.com>
2024-11-15 09:28:18 +0000

    add atlas db

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const port = 3000
 
 const Swagger = SwaggerDocs(app)
 
-mongoose.connect("mongodb://localhost:27017/PresbyDb").then(()=>{
+mongoose.connect("mongodb+srv://owusu2020emmanuel:Emre@19.@cluster0.1nfp2.mongodb.net/PresbyDB?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
     console.log('Db connected')
     app.listen( port,()=>{
     console.log('Server is up and running')  
