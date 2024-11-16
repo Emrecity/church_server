30daa4c2d2e3f4d6ca71b78c9b4494401bbbdb17
emrecity <owusu2020emmanuel@gmail.com>
2024-11-16 01:56:49 +0000

    removed db

--- index.js
+++ index.js
@@ -31,15 +31,17 @@ const port = 3000
 const Swagger = SwaggerDocs(app)
 
 
-mongoose.connect('mongodb://localhost:27017/PresbyDb').then(()=>{
-    console.log('Db connected')
-    app.listen( port,()=>{
-        console.log('Server is up and running')  
-        Swagger  
-    })
+// mongoose.connect('mongodb://localhost:27017/PresbyDb').then(()=>{
+//     console.log('Db connected')
+   
 
-}).catch((err)=>{
-    console.log(err)
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+app.listen( port,()=>{
+    console.log('Server is up and running')  
+    Swagger  
 })
 
 app.all('*',(req,res,next)=>{
