65c1685a24f4a154ab432b55d118c15dffb58de5
emrecity <owusu2020emmanuel@gmail.com>
2024-11-16 02:09:28 +0000

    added atlas db

--- index.js
+++ index.js
@@ -31,18 +31,18 @@ const port = 3000
 const Swagger = SwaggerDocs(app)
 
 
-// mongoose.connect('mongodb://localhost:27017/PresbyDb').then(()=>{
-//     console.log('Db connected')
-   
+mongoose.connect('mongodb+srv://owusu2020emmanuel:Emre@19.@cluster0.1nfp2.mongodb.net/PresbyDB?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
+    console.log('Db connected')
+    app.listen( port,()=>{
+        console.log('Server is up and running')  
+        Swagger  
+    })
 
-// }).catch((err)=>{
-//     console.log(err)
-// })
-
-app.listen( port,()=>{
-    console.log('Server is up and running')  
-    Swagger  
+}).catch((err)=>{
+    console.log(err)
 })
+
+
 
 app.all('*',(req,res,next)=>{
     const err = new CustomError(`can't find ${req.originalUrl} on server`,404)
