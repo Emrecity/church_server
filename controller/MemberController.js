b7d686daed5f0ea0adaf3e8b7e2338e5de253f7c
emrecity <owusu2020emmanuel@gmail.com>
2024-12-11 00:42:22 +0000

    fixed loginMember bug

--- controller/MemberController.js
+++ controller/MemberController.js
@@ -142,7 +142,7 @@ exports.deleteMember = asyncErrorHandler(async(req,res,next)=>{
 
 exports.loginMember = asyncErrorHandler(async (req,res)=>{
     const {firstname,phone} = req.body
-    const response = await Member.find({firstname:firstname,phone:phone})
+    const response = await Member.findOne({firstname:firstname,phone:phone})
     if(response){
         res.status(200).json({
             status:'success',
